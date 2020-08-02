827071416afa50bb2c32f75fe9fa6c953b297352
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-08-02 19:05:09 +0200

    File formatting

---
+++ src/Snake.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Snake() {
+  return (
+    <div className="Snake">
+      Test text
+    </div>
+  );
+}
+
+export default Snake;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Snake from './Snake';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Snake />
   </React.StrictMode>,
   document.getElementById('root')
 );
