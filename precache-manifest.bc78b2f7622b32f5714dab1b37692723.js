bfd22b2a8051df2bad62b61c44840e5f2b8e96dd
Zoltan Vegh <43003580+zotyovegh@users.noreply.github.com>
2020-09-13 19:41:04 +0200

    Updates

---
+++ precache-manifest.bc78b2f7622b32f5714dab1b37692723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb59f5bd2cfd9c153d5b8902d02968ea",
+    "url": "/Snake/index.html"
+  },
+  {
+    "revision": "fbfb7f8347a5a53756cd",
+    "url": "/Snake/static/css/main.0338cc16.chunk.css"
+  },
+  {
+    "revision": "161eebb0185ad66bd779",
+    "url": "/Snake/static/js/2.275b3820.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Snake/static/js/2.275b3820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbfb7f8347a5a53756cd",
+    "url": "/Snake/static/js/main.e662ce33.chunk.js"
+  },
+  {
+    "revision": "7f650829b46b4335ad15",
+    "url": "/Snake/static/js/runtime-main.01a72eab.js"
+  }
+]);
